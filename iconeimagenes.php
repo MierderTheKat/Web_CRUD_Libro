77494b745582f358ea6c4b4fe119de818e7376e7
MierderTheKat <lamierdasjj2@hotmail.com>
2022-04-06 02:34:15 -0500

    edit pw

--- iconeimagenes.php
+++ iconeimagenes.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost","root","lidiaoro.com","imagenes");
+$mysqli = new mysqli("localhost","root","","imagenes");
 
 if($mysqli->connect_error){
     echo "Ocurrió un error en al conexión: " . $mysqli->connect_error;
